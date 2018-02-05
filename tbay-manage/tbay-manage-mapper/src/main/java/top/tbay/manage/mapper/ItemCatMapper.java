3d7cf651e1de8a1f71559170775573a52afb68dc
kuangwu <neteasy571@163.com>
2018-02-05 10:26:21 +0800

    initialize commit,Merge 2017 code.

---
+++ tbay-manage/tbay-manage-mapper/src/main/java/top/tbay/manage/mapper/ItemCatMapper.java
@@ -0,0 +1,22 @@
+package top.tbay.manage.mapper;
+
+import com.github.abel533.mapper.Mapper;
+import top.tbay.manage.pojo.ItemCat;
+
+import java.util.List;
+
+public interface ItemCatMapper extends Mapper<ItemCat> {
+//    int deleteByPrimaryKey(Long id);
+//
+//    int insert(ItemCat record);
+//
+//    int insertSelective(ItemCat record);
+//
+//    ItemCat selectByPrimaryKey(Long id);
+//
+//    int updateByPrimaryKeySelective(ItemCat record);
+//
+//    int updateByPrimaryKey(ItemCat record);
+//
+//    List<ItemCat> selectItemCategroyList(ItemCat record);
+}
---
+++ tbay-manage/tbay-manage-service/src/main/java/top/tbay/manage/service/ItemCatService.java
@@ -0,0 +1,23 @@
+package top.tbay.manage.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import top.tbay.manage.mapper.ItemCatMapper;
+import top.tbay.manage.pojo.ItemCat;
+
+import java.util.List;
+
+/**
+ * @author 571
+ * @create 2017-11-22 13:44:12
+ */
+@Service
+public class ItemCatService extends BaseService<ItemCat>{
+
+    @Autowired
+    private ItemCatMapper itemCatMapper;
+
+    public List<ItemCat> queryItemCategroyList(ItemCat record) {
+        return itemCatMapper.select(record);
+    }
+}
